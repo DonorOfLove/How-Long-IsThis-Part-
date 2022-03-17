f2280893fa0f52f4105be52668b43b397fcdf4b5
DonorOfLove <StasEzovitov89@gmail.com>
2022-03-17 20:13:54 +0300

    ??

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-var bpm = document.getElementById('bpm');
-var quartes = document.getElementById('quartes');
-var tacts = document.getElementById("tacts");
-var res = document.getElementById("second");
-var debounce = function (fn, ms) {
-    if (ms === void 0) { ms = 3000; }
-    var timeoutId;
-    return function () {
-        var _this = this;
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
+const bpm = document.getElementById('bpm');
+const quartes = document.getElementById('quartes');
+const tacts = document.getElementById("tacts");
+const res = document.getElementById("second");
+const tactsPlace = document.getElementById("tactsPlace");
+const trackName = document.getElementById("track");
+const inputs = [quartes, tacts, bpm];
+const debounce = (fn, ms = 3000) => {
+    let timeoutId;
+    return function (...args) {
         clearTimeout(timeoutId);
-        timeoutId = setTimeout(function () { return fn.apply(_this, args); }, ms);
+        timeoutId = setTimeout(() => fn.apply(this, args), ms);
     };
 };
-function debFunc() {
+fetch(`https://api.getsongbpm.com/search/?api_key=90719da58353ff9ff991b71f33c07a3f&type=lookup:green+day`)
+    .then((response) => {
+    return response.json();
+})
+    .then((data) => {
+    console.log(data);
+});
+function calcTime() {
     if (bpm.value.length > 0 && quartes.value.length > 0 && tacts.value.length > 0) {
-        var asf = Number(quartes.value) / (Number(bpm.value) / 60) * Number(tacts.value);
+        let asf = Number(quartes.value) / (Number(bpm.value) / 60) * Number(tacts.value);
         res.innerHTML = JSON.stringify(Math.floor(asf));
+        tactsPlace.innerHTML = tacts.value;
     }
 }
-bpm.addEventListener("keyup", debounce(function () {
-    debFunc();
-}, 300));
-quartes.addEventListener("keyup", debounce(function () {
-    debFunc();
-}, 300));
-tacts.addEventListener("keyup", debounce(function () {
-    debFunc();
-}, 300));
+for (let el of inputs) {
+    el.addEventListener("keyup", debounce(() => {
+        calcTime();
+    }, 500));
+}
+//# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -2,42 +2,41 @@ const bpm=document.getElementById('bpm') as HTMLInputElement
 const quartes=document.getElementById('quartes') as HTMLInputElement
 const tacts=document.getElementById("tacts") as HTMLInputElement
 const res=document.getElementById("second") as HTMLInputElement
+const tactsPlace=document.getElementById("tactsPlace") as HTMLElement
+const trackName=document.getElementById("track") as HTMLInputElement
+const inputs:HTMLInputElement[]= [quartes,tacts,bpm]
+
 
 const debounce = (fn: Function, ms = 3000) => {
     let timeoutId: ReturnType<typeof setTimeout>;
-
     return function (this: any, ...args: any[]) {
         clearTimeout(timeoutId);
-
         timeoutId = setTimeout(() => fn.apply(this, args), ms);
     };
 };
 
-function debFunc() {
+fetch(`https://api.getsongbpm.com/search/?api_key=90719da58353ff9ff991b71f33c07a3f&type=lookup:green+day`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
 
+function calcTime() {
     if (bpm.value.length>0&&quartes.value.length>0&&tacts.value.length>0){
         let asf:number=Number(quartes.value)/(Number(bpm.value)/60)*Number(tacts.value)
         res.innerHTML=JSON.stringify(Math.floor(asf))
+        tactsPlace.innerHTML=tacts.value
     }
 }
-bpm.addEventListener(
-    "keyup",
-    debounce(() => {
-     debFunc()
-    }, 300)
-);
-quartes.addEventListener(
-    "keyup",
-    debounce(() => {
-        debFunc()
-    }, 300)
-);
-
-tacts.addEventListener(
-    "keyup",
-    debounce(() => {
-        debFunc()
-    }, 300)
-);
+for (let el of inputs){
+   el.addEventListener(
+       "keyup",
+       debounce(() => {
+           calcTime()
+       }, 500)
+   );
+}
 
 
