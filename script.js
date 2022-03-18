e3435238c5a5a582ec2b87fd5e2409ee8ce0b69d
DonorOfLove <83128004+DonorOfLove@users.noreply.github.com>
2022-03-18 15:04:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const tacts = document.getElementById("tacts");
 const res = document.getElementById("second");
 const tactsPlace = document.getElementById("tactsPlace");
 const trackName = document.getElementById("track");
-const list = document.getElementById('<list>');
+const list = document.getElementById('list');
+const loadAnimation = document.createElement("img");
+loadAnimation.src = "img/load.gif";
+let sigStore = quartes.value;
 const inputs = [quartes, tacts, bpm];
 const debounce = (fn, ms = 3000) => {
     let timeoutId;
@@ -14,32 +17,52 @@ const debounce = (fn, ms = 3000) => {
     };
 };
 trackName.addEventListener("keyup", debounce(() => {
+    list.innerHTML = '';
+    list.appendChild(loadAnimation);
     fetch(`https://api.getsongbpm.com/search/?api_key=90719da58353ff9ff991b71f33c07a3f&type=song&lookup=${trackName.value}`)
         .then((response) => {
         return response.json();
     })
         .then((data) => {
+        list.innerHTML = '';
         console.log(data);
         for (const el of data.search) {
-            const song = document.createElement('option');
+            const song = document.createElement('p');
             song.innerHTML = `${el.title} - ${el.artist.name}`;
             list.appendChild(song);
-            song.addEventListener('mousemove', () => {
-                console.log('s');
-            });
+            song.addEventListener('click', () => findById(el.id));
         }
-        console.log(data.search);
     });
 }, 1000));
+function findById(id) {
+    fetch(`https://api.getsongbpm.com/song/?api_key=90719da58353ff9ff991b71f33c07a3f&id=${id}`)
+        .then((response) => {
+        return response.json();
+    })
+        .then((data) => {
+        console.log(data.song);
+        bpm.value = data.song.tempo;
+        quartes.value = data.song.time_sig;
+        calcTime();
+    });
+}
+function calcQuartes(sig) {
+    if (sig[2] == 4) {
+        return sig[0];
+    }
+    else {
+        return sig[0] / sig[2] * 4;
+    }
+}
 function calcTime() {
     if (bpm.value.length > 0 && quartes.value.length > 0 && tacts.value.length > 0) {
-        let asf = Number(quartes.value) / (Number(bpm.value) / 60) * Number(tacts.value);
-        res.innerHTML = JSON.stringify(Math.floor(asf));
+        let resNum = calcQuartes(quartes.value) / (Number(bpm.value) / 60) * Number(tacts.value);
+        res.innerHTML = JSON.stringify(Math.floor(resNum));
         tactsPlace.innerHTML = tacts.value;
     }
 }
 for (let el of inputs) {
-    el.addEventListener("keyup", debounce(() => {
+    el.addEventListener("input", debounce(() => {
         calcTime();
     }, 500));
 }
--- script.ts
+++ script.ts
@@ -1,11 +1,14 @@
-const bpm=document.getElementById('bpm') as HTMLInputElement
-const quartes=document.getElementById('quartes') as HTMLInputElement
-const tacts=document.getElementById("tacts") as HTMLInputElement
-const res=document.getElementById("second") as HTMLInputElement
-const tactsPlace=document.getElementById("tactsPlace") as HTMLElement
-const trackName=document.getElementById("track") as HTMLInputElement
-const list = document.getElementById('<list>')
-const inputs:HTMLInputElement[]= [quartes,tacts,bpm]
+const bpm = document.getElementById('bpm') as HTMLInputElement
+const quartes = document.getElementById('quartes') as HTMLInputElement
+const tacts = document.getElementById("tacts") as HTMLInputElement
+const res = document.getElementById("second") as HTMLInputElement
+const tactsPlace = document.getElementById("tactsPlace") as HTMLElement
+const trackName = document.getElementById("track") as HTMLInputElement
+const list = document.getElementById('list')
+const loadAnimation=document.createElement("img")
+loadAnimation.src="img/load.gif"
+let sigStore:string = quartes.value
+const inputs: HTMLInputElement[] = [quartes, tacts, bpm]
 
 const debounce = (fn: Function, ms = 3000) => {
     let timeoutId: ReturnType<typeof setTimeout>;
@@ -15,38 +18,62 @@ const debounce = (fn: Function, ms = 3000) => {
     };
 };
 
-trackName.addEventListener("keyup", debounce(()=>{fetch(`https://api.getsongbpm.com/search/?api_key=90719da58353ff9ff991b71f33c07a3f&type=song&lookup=${trackName.value}`)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data)
-for (const el of data.search){
-    const song=document.createElement('option')
-    song.innerHTML=`${el.title} - ${el.artist.name}`
+trackName.addEventListener("keyup", debounce(() => {
+    list.innerHTML=''
+    list.appendChild(loadAnimation)
+    fetch(`https://api.getsongbpm.com/search/?api_key=90719da58353ff9ff991b71f33c07a3f&type=song&lookup=${trackName.value}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            list.innerHTML=''
+            console.log(data)
+            for (const el of data.search) {
+                const song = document.createElement('p')
+                song.innerHTML = `${el.title} - ${el.artist.name}`
+                list.appendChild(song)
+                song.addEventListener('click', () => findById(el.id))
+            }
+        });
+}, 1000))
 
-    list.appendChild(song)
-    song.addEventListener('mousemove',()=>{
-        console.log('s')})
+function findById(id: string) {
+
+    fetch(`https://api.getsongbpm.com/song/?api_key=90719da58353ff9ff991b71f33c07a3f&id=${id}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data.song)
+            bpm.value = data.song.tempo
+            quartes.value=data.song.time_sig
+          calcTime()
+        })
 }
-        console.log(data.search);
-    });
-},1000))
 
-function calcTime() {
-    if (bpm.value.length>0&&quartes.value.length>0&&tacts.value.length>0){
-        let asf:number=Number(quartes.value)/(Number(bpm.value)/60)*Number(tacts.value)
-        res.innerHTML=JSON.stringify(Math.floor(asf))
-        tactsPlace.innerHTML=tacts.value
+function calcQuartes(sig) {
+    if (sig[2] == 4) {
+        return sig[0]
+    } else {
+        return sig[0] / sig[2] * 4
     }
 }
-for (let el of inputs){
-   el.addEventListener(
-       "keyup",
-       debounce(() => {
-           calcTime()
-       }, 500)
-   );
+
+function calcTime() {
+    if (bpm.value.length > 0 && quartes.value.length > 0 && tacts.value.length > 0) {
+        let resNum: number = calcQuartes(quartes.value) / (Number(bpm.value) / 60) * Number(tacts.value)
+        res.innerHTML = JSON.stringify(Math.floor(resNum))
+        tactsPlace.innerHTML = tacts.value
+    }
+}
+
+for (let el of inputs) {
+    el.addEventListener(
+        "input",
+        debounce(() => {
+            calcTime()
+        }, 500)
+    );
 }
 
 
